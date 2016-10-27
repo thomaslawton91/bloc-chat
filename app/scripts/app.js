3ea5d3bd331254521251f4b9e90caa7ac5c1df67
deepanchor12 <thomaslawton91@gmail.com>
2016-10-27 14:13:08 -0400

    Working in story 1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,30 @@
+var config = {
+    apiKey: "AIzaSyCm4s2lame6SbSmAqvBt4da_CoAvexdtHM",
+    authDomain: "bloc-chat-8fa2b.firebaseapp.com",
+    databaseURL: "https://bloc-chat-8fa2b.firebaseio.com",
+    storageBucket: "bloc-chat-8fa2b.appspot.com",
+    messagingSenderId: "423994139017"
+  };
+firebase.initializeApp(config);
+
 (function () {
     function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
                 enabled: true,
                 requireBase: false
-            });
-
+        });
+        
         $stateProvider
-            .state('landing', {
+            .state('home', {
                 url: '/',
-                controller: 'LandingCtrl as landing',
-                templateUrl: '/templates/landing.html'
-            })
-            .state('album', {
-                url: '/album',
-                controller: ,
-                templateUrl: 
-            })
-            .state('collection', {
-                url: '/collection',
-                controller: 'CollectionCtrl as collection',
-                templateUrl: '/templates/collection.html'
+                controller: 'RoomCtrl as home',
+                templateUrl: '/templates/home.html'
             });
-    }
+    }    
+    
     angular
-        .module("blocChat", ["firebase", "ui.router"]);
-    .config(config);
+        .module('blocChat', ['firebase', 'ui.router'])
+        .config(config);
 })();
+
