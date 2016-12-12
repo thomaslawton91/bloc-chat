6ef510e69e2b1dfedddd41eb0888d6ea4ec7d164
deepanchor12 <thomaslawton91@gmail.com>
2016-12-12 18:01:07 -0500

    Message send working

--- dist/scripts/controllers/RoomCtrl.js
+++ dist/scripts/controllers/RoomCtrl.js
@@ -1,27 +1,39 @@
 (function () {
-    function RoomCtrl(Room, Message, $uibModal, $scope) {
+    function RoomCtrl(Room, Message, $uibModal, $scope, $cookies) {
         this.list = Room.all;
         this.openModal = function () {
             console.log('Modal open');
-                var modalInstance = $uibModal.open({
+            var modalInstance = $uibModal.open({
                 animation: true,
                 size: 'sm',
                 templateUrl: '/templates/chatModal.html',
                 controller: 'ChatModalCtrl'
             });
-                modalInstance.result.then(function(selectedItem) {
-                    this.selected = selectedItem;
-                    Room.addRoom(selectedItem);
-                     
-                });
-            };
-        this.selectedRoom = function(rooms) {
+            modalInstance.result.then(function (selectedItem) {
+                this.selected = selectedItem;
+                Room.addRoom(selectedItem);
+
+            });
+        };
+        this.selectedRoom = function (rooms) {
             this.chosenRoom = rooms;
             this.messages = Message.getByRoomId(rooms.$id);
             return this.messages;
         };
-        };
+        this.content = '';
+        var currentTime = new Date();
+        var sentTime = '' + currentTime.getHours() + ':' + currentTime.getMinutes();
+        this.sendMessage = function() {
+            Message.send({
+                content: this.content,
+                username: $cookies.get('blocChatCurrentUser'),
+                roomId: this.chosenRoom.$id,
+                sentAt: sentTime
+            });
+            console.log('clicked');
+    };
+    };
     angular
         .module('blocChat')
-        .controller('RoomCtrl', ['Room', 'Message', '$uibModal', '$scope', RoomCtrl]);
+        .controller('RoomCtrl', ['Room', 'Message', '$uibModal', '$scope', '$cookies', RoomCtrl]);
 })();
--- dist/scripts/services/Message.js
+++ dist/scripts/services/Message.js
@@ -1,6 +1,7 @@
 (function() {
   function Message($firebaseArray) {
-    var ref = firebase.database().ref().child('messages').orderByChild('roomId');
+    var roomRef = firebase.database().ref().child('messages').orderByChild('roomId');
+    var ref = firebase.database().ref().child('messages')
     // getvar getMessages = Room.getMessages;
     var messages = $firebaseArray(ref);  
     var getMessages = function(roomId) {
@@ -8,7 +9,10 @@
         }
     return {
         getByRoomId: function(roomId) {
-            return $firebaseArray(ref.equalTo(roomId));
+            return $firebaseArray(roomRef.equalTo(roomId));
+        },
+        send: function(newMessage) {
+            messages.$add(newMessage);
         }
     };
   }
